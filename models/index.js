c820a5bdcb0cd64bf3e15e6a530d28953e66d21d
Mohamed Habshey <Mohamed.habshey10@gmail.com>
2017-01-29 16:25:18 +0200

    change to PostgreSQL

--- models/index.js
+++ models/index.js
@@ -6,7 +6,12 @@ var Sequelize = require("sequelize");
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+var sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }
+});
 var db        = {};
 
 fs
